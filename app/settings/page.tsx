6dd8e1010e11f597517109d7e88a5c228c3a7e93
SamarAbbaas <fsc8101@gmail.com>
2026-07-25 23:02:21 +0500

    feat: update welcome message on Settings Page for clarity

--- app/settings/page.tsx
+++ app/settings/page.tsx
@@ -2,7 +2,9 @@ export default function settingsPage() {
   return (
     <div className="flex flex-col gap-4 min-h-screen items-center justify-center p-4">
       <h1 className="text-2xl font-bold">Settings Page</h1>
-      <p>Welcome to the Settings Page!</p>
+      <p>Welcome to the Settings Page!
+        This is setting page and available sooon 
+      </p>
     </div>
   );
 }
