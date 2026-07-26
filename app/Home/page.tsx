95c726ddf12a615eea2bd891db974129318910ac
SamarAbbaas <fsc8101@gmail.com>
2026-07-26 22:02:41 +0500

    feat: update Home page message to indicate availability status

--- app/Home/page.tsx
+++ app/Home/page.tsx
@@ -3,7 +3,7 @@ export default function Home() {
   return (
     <>
     <div className="flex justify-center items-center min-h-screen bg-blend-color primary " >
-        Welcome to the Home Page!
+        Welcome to the Home Page! home page is available soon
     </div>
     </>
   );
