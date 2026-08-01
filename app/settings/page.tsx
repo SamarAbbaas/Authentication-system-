85590d443790620830a731cdca0b8e670c4d8df4
SamarAbbaas <fsc8101@gmail.com>
2026-08-01 20:33:39 +0500

    feat: refactor SettingsPage component with improved logout functionality and UI enhancements

--- app/settings/page.tsx
+++ app/settings/page.tsx
@@ -1,10 +1,48 @@
-export default function settingsPage() {
+'use client';
+import { Button } from "@/components/ui/button";
+import { LogOut } from "lucide-react";
+import Link from "next/link";
+import toast from 'react-hot-toast';
+import { supabase } from '@/lib/supabase';
+import { useRouter } from 'next/navigation';
+
+
+
+export default function SettingsPage() {
+    const router = useRouter();
+
+ const handleLogout = async () => {
+    try {
+      await supabase.auth.signOut();
+      toast.success('Logged out successfully');
+      router.push('/signin');
+    } catch (error) {
+      toast.error('Error logging out');
+    }
+  };
+
+  
   return (
     <div className="flex flex-col gap-4 min-h-screen items-center justify-center p-4">
       <h1 className="text-2xl font-bold">Settings Page</h1>
-      <p>Welcome to the Settings Page!
-        This is setting page and available sooon 
-      </p>
+     
+        <Link href="/forgot">
+             <Button >Reset your Pasword </Button>
+        </Link>
+         <Link href="/upload">
+             <Button > Change Profile Picture </Button>
+        </Link>
+        
+ <Button 
+              variant="ghost" 
+              size="sm" 
+              onClick={handleLogout}
+              className="flex items-center gap-2 text-destructive hover:bg-destructive/10 hover:text-destructive"
+            >
+              <LogOut className="size-4" />
+              Logout
+            </Button>
+     
     </div>
   );
 }
